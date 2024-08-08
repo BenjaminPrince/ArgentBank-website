91126f1d87c73676182ab4a463cdd78df18fe9d0
BenjaminPrince <prince-benjamin@hotmail.com>
2024-08-08 15:55:42 +0200

    app(bank): add front react json

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Features from './components/features';
+import SignIn from './components/SignIn';
+import User from './components/User';
+import Footer from './components/Footer';
+import '../src/assets/css/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<>
+            <Hero />
+            <Features />
+          </>} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
